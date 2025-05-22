59add75155fc683ad13afc819edea6a20c65ad6f
chinniroy <yadlachinni327@gmail.com>
2025-05-22 23:36:00 +0530

    conditionalOddSeries.java

--- conditionalOddSeries.java
+++ conditionalOddSeries.java
@@ -1,1 +1,19 @@
+// Language: Java
+// Program-3: Conditional odd series depending on even/odd input
 
+import java.util.*;
+
+public class Program3 {
+    public static List<Integer> generateConditionalOddSeries(int x) {
+        int count = (x % 2 != 0) ? x : x - 1;
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < (count + 1) / 2; i++) {
+            result.add(2 * i + 1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Output: " + generateConditionalOddSeries(4)); // [1, 3, 5]
+    }
+}
