ed718f3f17207755ac6f21d4d30ba397c4ebe113
chinniroy <yadlachinni327@gmail.com>
2025-05-22 23:35:20 +0530

    oddSeriesGenerator.java

--- oddSeriesGenerator.java
+++ oddSeriesGenerator.java
@@ -1,1 +1,18 @@
+// Language: Java
+// Program-2: Generate odd number series up to count 
 
+import java.util.*;
+
+public class Program2 {
+    public static List<Integer> generateOddSeries(int a) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < a; i++) {
+            result.add(2 * i + 1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Output: " + generateOddSeries(4));
+    }
+}
